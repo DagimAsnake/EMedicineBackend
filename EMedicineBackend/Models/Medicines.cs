ec2cff087ab37daf5ed2e051af186a0e71c88118
DagimAsnake <dagimasnake1@gmail.com>
2023-11-03 00:04:03 -0700

    finished creating models

---
+++ EMedicineBackend/Models/Medicines.cs
@@ -0,0 +1,21 @@
+﻿namespace EMedicineBackend.Models
+{
+    public class Medicines
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Manugacturer { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public decimal Discount { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime ImageUrl { get; set; }
+
+        public int Status { get; set; }
+    }
+}
--- EMedicineBackend/Models/Response.cs
+++ EMedicineBackend/Models/Response.cs
@@ -6,8 +6,22 @@
 
         public string StatusMessage { get; set; }
 
-        public List<Users> listUsers { get; set; }
+        public List<Users> ListUsers { get; set; }
 
         public Users user { get; set; }
+
+        public List<Medicines> ListMedicines { get; set; }
+
+        public Medicines medicine { get; set; }
+
+        public List<Cart> ListCart { get; set; }
+
+        public List<Orders> ListOrders { get; set; }
+
+        public Orders order { get; set; }
+
+        public List<OrderItems> ListItems { get; set; }
+
+        public OrderItems orderItem { get; set; }
     }
 }
